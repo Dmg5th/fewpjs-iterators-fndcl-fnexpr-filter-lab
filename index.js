c6d3dba50de4b39e51b4731032bf997ce89cffb0
Dmg5th <dmgould310@gmail.com>
2021-01-13 15:50:35 -0500

    commit tree 5ae711896b0e6462715187c5ad2ad0a15c528201

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solution here
+let drivers = ["Daniel", "Jim", "Tony", "Chris", "daniel"]
+
+
+function findMatching(array, string){
+    let newArray = array.filter(names =>  names.toLowerCase() === string.toLowerCase())
+    return newArray 
+}
+
+function fuzzyMatch(array, string) {
+    let newArray = array.filter(names => names.startsWith(string))
+    return newArray   
+}
+
+function matchName(array, string){
+    return array.filter(obj => obj.name === string)
+}
